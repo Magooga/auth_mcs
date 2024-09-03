e7dd134a3d09e2cefc999252bb4ebf44346dd138
eig <eig@mail.ru>
2024-09-03 21:13:02 +0300

    add docker-compose

--- Infrastructure/Infrastructure.EntityFramework/DataBaseContext.cs
+++ Infrastructure/Infrastructure.EntityFramework/DataBaseContext.cs
@@ -14,6 +14,7 @@ namespace Infrastructure.EntityFramework
 
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
         {
+            Database.Migrate();
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
